d5fd3259f78f3240f92e8b45f281c9b94a2d4616
Danny <d.fachal@udc.es>
2025-12-01 17:06:50 +0100

    translated help

--- help.c
+++ help.c
@@ -138,7 +138,7 @@ void printHelp(){
 	};
 	col = 0;
 	//! marks the end of the list, and so must always be at the end
-	printf("Comandos disponibles:\n");
+	printf("Available commands:\n");
 	for(i = 0; strcmp(allCommands[i],"!"); i++){
 		if (col >= 3){
 			col = 0;
@@ -157,46 +157,46 @@ void printHelp(){
 
 void helpAuthors(){
 	printf("\t|authors [-n][-l]\n");
-	printf("Muestra los nombres y/o logins de los autores\n\n");
+	printf("Show authors name and/or e-mail\n\n");
 }
 
 
 void helpPid(){
 	printf("\t|pid [-p]\n");
-	printf("Muestra el pid del shell o de su proceso padre\n\n");
+	printf("Show shell's PID or the parent process's PID\n\n");
 }
 
 
 void helpPPid(){
 	printf("\t|ppid\n");
-	printf("Muestra el pid del proceso padre del shell\n\n");
+	printf("Show shell's parent process's PID\n\n");
 }
 
 
 void helpCd(){
 	printf("\t|cd [dir]\n");
-	printf("Cambia (o muestra) el directorio actual del shell\n\n");
+	printf("Changes (or shows) current working directory\n\n");
 }
 
 
 void helpDate(){
-	printf("\t|date [-t][-h]\n");
-	printf("Muestra la fecha y/o la hora actual\n\n");
+	printf("\t|date [-d][-h]\n");
+	printf("Shows current date [-d] and/or time [-t]\n\n");
 }
 
 
 void helpHistoric(){
 	printf("\t|historic [-c|N|-N]\n");
-	printf("Muestra (o borra)el histórico de comandos");
+	printf("Shows (or deletes) history of commands. Can also execute a command in the history");
 	printf("\t-c: Borra el histórico\n");
 	printf("\t-N: Muestra los N últimos comandos\n");
-	printf("\tN: Ejecuta de nuevo el comando N de la lista \n\t\t(no permite ejecución de comandos historic)\n\n");
+	printf("\tN: Executes again command number N in the list \n\t\t(does not allow executing a \"historic\" command from the list)\n\n");
 }
 
 
 void helpOpen(){
 	printf("\t|open [file] [mode]\n");
-	printf("Abre el archivo \"file\" y lo añade a la lista de ficheros abiertos \nSin argumentos imprime la lista\n\t[mode] permite los siguientes valores de dos caracteres:\n");
+	printf("Opens \"file\" and adds it to the list of open files\nWithout arguments it shows the list of open files\n\t[mode] allows the following open modes:\n");
 	printf("\n\tcr  = O_CREAT");
 	printf("\tap  = O_APPEND");
 	printf("\n\tex  = O_EXCL");
@@ -210,43 +210,43 @@ void helpOpen(){
 
 void helpClose(){
 	printf("\t|close df\n");
-	printf("Cierra el descriptor df y elimina el correspondiente fichero de la lista de ficheros abiertos\n\n");
+	printf("Closes file with file descriptor \"df\" and eliminates the element from the open file list\n\n");
 }
 
 
 void helpDup(){
 	printf("\t|dup df\n");
-	printf("Duplica el descriptor de fichero \"df\" y anade una nueva entrada a la lista ficheros abiertos\n\n");
+	printf("Duplicates file with descriptor \"df\" also adding a new entry to the open file list\n\n");
 }
 
 
 void helpInfoSys(){
 	printf("\t|infosys\n");
-	printf("Muestra informacion de la maquina donde corre el shell\n\n");
+	printf("Shows system information\n\n");
 }
 
 
 void helpHelp(){
 	printf("\t|help [cmd]\n");
-	printf("Muestra todos los comandos disponibles por pantalla\n\t[cmd] muestra información de un comando específico\n\n");
+	printf("Shows all available commands.\n\t[cmd] shows information about a specific command and its parameters\n\n");
 }
 
 
 void helpQuit(){
 	printf("\t|quit\n");
-	printf("Termina la ejecucion del shell\n\n");
+	printf("Ends shell execution\n\n");
 }
 
 
 void helpExit(){
 	printf("\t|exit\n");
-	printf("Termina la ejecucion del shell\n\n");
+	printf("Ends shell execution\n\n");
 }
 
 
 void helpBye(){
 	printf("\t|bye\n");
-	printf("Termina la ejecucion del shell\n\n");
+	printf("Ends shell execution\n\n");
 }
 
 
@@ -254,114 +254,116 @@ void helpBye(){
 
 void helpMakeFile(){
 	printf("\t|makefile [name]\n");
-	printf("Crea un fichero de nombre \"name\"\n");
-	printf("Si no hay ningún nombre de archivo, muestra el directorio actual del shell\n\n");
+	printf("Creates a new file called \"name\"\n");
+	printf("If no file name is provided, prints current working directory\n\n");
 }
 
 void helpMakeDir(){
 	printf("\t|makedir [name]\n");
-	printf("Crea un directorio de nombre \"name\"\n");
-	printf("Si no hay ningún nombre de archivo, muestra el directorio actual del shell\n\n");
+	printf("Creates a new directory called \"name\"\n");
+	printf("If no file name is provided, prints current working directory\n\n");
 }
 
 void helpListFile(){
 	printf("\t|listfile [-long] [-link] [-acc] [name1] [name2]... \n");
-	printf("Lista ficheros con su información (tamaño y nombre)\n");
-	printf("\t-long: Listado largo (último acceso;num.inodo;permisos;userID;groupID;num.hardlinks\n");
-	printf("\t-acc: Lista la fecha y hora de último acceso\n");
-	printf("\t-link: Si el nombre corresponde a un enlace simbólico, muestra el path contenido\n");
-	printf("\tname: Puede haber más de un nombre de archivo a listar, pero deben estar siempre al final\n");
-	printf("Si no hay ningún nombre de archivo, muestra el directorio actual del shell\n\n");
+	printf("Lists files and their information (name and size)\n");
+	printf("\t-long: Lost listing (last access; inode number; permissions; userID; groupID; number of hardlinks\n");
+	printf("\t-acc: Lists date and time of last access\n");
+	printf("\t-link: If the file is a symbolic link, shows contained path\n");
+	printf("\tname: May contain more than one file name, but they must be at the end of the command and contain no spaces\n");
+	printf("If no file name is provided, prints current working directory\n\n");
 	
 }
 
 void helpCWD(){
 	printf("\t|cwd\n");
-	printf("Muestra el directorio actual del shell\n\n");
+	printf("Prints current working directory\n\n");
 }
 
 void helpListDir(){
 	printf("\t|{listdir/ls} [-hid][-long][-acc][-link][dir1][dir2]\n");
-	printf("Lista los ficheros mostrados dentro del directorio \"dir\" con su información)\n");
-	printf("\t-hid: incluye los ficheros ocultos\n");
-	printf("\t-long: Listado largo (último acceso;num.inodo;permisos;userID;groupID;num.hardlinks\n");
-	printf("\t-acc: Lista la fecha y hora de último acceso\n");
-	printf("\t-link: Si el nombre corresponde a un enlace simbólico, muestra el path contenido\n");
-	printf("\tdir: Puede haber más de un nombre de archivo a listar, pero deben estar siempre al final\n");
-	printf("Si no hay ningún nombre de directorio, se listarán los contenidos del directorio actual\n\n");
+	printf("Lists files withing the directory \"dir\" (or current directory if none is provided) with their information\n");
+	printf("\t-hid: Show hidden files \n");
+	printf("\t-long: Long listing (last access; inode number; permissions; userID; groupID; number of hardlinks\n");
+	printf("\t-acc: Lists date and time of last access\n");
+	printf("\t-link: If the file is a symbolic link, shows contained path\n");
+	printf("\tname: May contain more than one file name, but they must be at the end of the command and contain no spaces\n");
+	printf("If no file name is provided, prints current working directory\n\n");
 }
 
 void helpRecList(){
 	printf("\t|reclist [-hid][-long][-acc][-link][dir1][dir2]\n");
-	printf("Lista los ficheros mostrados dentro del directorio \"dir\" con su información)\n");
-	printf("Adicionalmente, mostrará después recursivamente los archivos contenidos dentro de cada directorio\n");
-	printf("\t-hid: incluye los ficheros ocultos\n");
-	printf("\t-long: Listado largo (último acceso;num.inodo;permisos;userID;groupID;num.hardlinks\n");
-	printf("\t-acc: Lista la fecha y hora de último acceso\n");
-	printf("\t-link: Si el nombre corresponde a un enlace simbólico, muestra el path contenido\n");
-	printf("\tdir: Puede haber más de un nombre de archivo a listar, pero deben estar siempre al final\n");
-	printf("Si no hay ningún nombre de directorio, muestra el directorio actual del shell\n\n");
+	printf("Lists files withing the directory \"dir\" (or current directory if none is provided) with their information\n");
+	printf("Additionally, it will print recursively all files contained within each directory afterwards\n");
+	printf("\t-hid: Show hidden files \n");
+	printf("\t-long: Long listing (last access; inode number; permissions; userID; groupID; number of hardlinks\n");
+	printf("\t-acc: Lists date and time of last access\n");
+	printf("\t-link: If the file is a symbolic link, shows contained path\n");
+	printf("\tname: May contain more than one file name, but they must be at the end of the command and contain no spaces\n");
+	printf("If no file name is provided, prints current working directory\n\n");
 }
 
 void helpRevList(){
 	printf("\t|revlist [-hid][-long][-acc][-link][dir1][dir2]\n");
-	printf("Lista los ficheros mostrados dentro del directorio \"dir\" con su información)\n");
-	printf("Adicionalmente, mostrará antes recursivamente los archivos contenidos dentro de cada directorio\n");
-	printf("\t-hid: incluye los ficheros ocultos\n");
-	printf("\t-long: Listado largo (último acceso;num.inodo;permisos;userID;groupID;num.hardlinks\n");
-	printf("\t-acc: Lista la fecha y hora de último acceso\n");
-	printf("\t-link: Si el nombre corresponde a un enlace simbólico, muestra el path contenido\n");
-	printf("\tdir: Puede haber más de un nombre de archivo a listar, pero deben estar siempre al final\n");
-	printf("Si no hay ningún nombre de directorio, muestra el directorio actual del shell\n\n");
+	printf("Lists files withing the directory \"dir\" (or current directory if none is provided) with their information\n");
+	printf("Additionally, it will print recursively all files contained within each directory beforehand\n");
+	printf("\t-hid: Show hidden files \n");
+	printf("\t-long: Long listing (last access; inode number; permissions; userID; groupID; number of hardlinks\n");
+	printf("\t-acc: Lists date and time of last access\n");
+	printf("\t-link: If the file is a symbolic link, shows contained path\n");
+	printf("\tname: May contain more than one file name, but they must be at the end of the command and contain no spaces\n");
+	printf("If no file name is provided, prints current working directory\n\n");
 }
 
 void helpErase(){
 	printf("\t|erase [name1][name2]\n");
-	printf("Borra ficheros o directorios vacíos.\n");
-	printf("Puede haber más de un nombre de archivo o directorio\n");
-	printf("Si no hay ningún nombre de directorio, muestra el directorio actual del shell\n\n");
+	printf("Deletes files or empty directories\n");
+	printf("May contain more than one name of file or directory\n");
+	printf("If no file or directory name is provided, prints current working directory\n\n");
 }
 
 void helpDelRec(){
 	printf("\t|delrec [name1][name2]\n");
-	printf("Borra ficheros o directorios no vacíos recursivamente.\n");
-	printf("Puede haber más de un nombre de archivo o directorio\n");
-	printf("Si no hay ningún nombre de directorio, muestra el directorio actual del shell\n\n");
+	printf("Deletes a file or a directory. It will delete recursively all contents of the directory\n");
+	printf("May contain more than one name of file or directory\n");
+	printf("If no file or directory name is provided, prints current working directory\n\n");
 }
 
 void helpAllocate(){
 	printf("\t|allocate [-malloc] [N]\n");
-	printf("\t-malloc N: Asigna un espacio en memoria de tamaño N\n");
-	printf("Sin argumentos muestra la tabla de espacio asignado\n\n");
+	printf("\t-malloc N: Assigns a space in memory of size N\n");
+	printf("Running without arguments will print the table of assigned memory spaces\n\n");
 }
 
 void helpDeallocate(){
 	printf("\t|deallocate <address>/[-malloc] [N]\n");
-	printf("\t<address>: Desasigna el espacio en memoria en la dirección <address>\n");
-	printf("\t-malloc N: Desasigna un espacio en memoria asignado con tamaño N\n");
-	printf("Sin argumentos muestra la tabla de espacio asignado\n\n");
+	printf("\t<address>: Deassigns the space in memory of <address>\n");
+	printf("\t-malloc N: Deassigns a space in memory of size N (only one if there are several)\n");
+	printf("Running without arguments will print the table of assigned memory spaces\n\n");
 }
 
 void helpMemFill(){
 	printf("\t|memfill <address> <size> <char>\n");
-	printf("Llena <size> bytes en memoria desde <address> con el caracter <char>\n\n");
+	printf("Fills <size> bytes in memory from <address> with the character <char>\n\n");
 }
 
 void helpMemDump(){
 	printf("\t|memdump <address> <size>\n");
-	printf("Muestra el contenido en memoria de <size> bytes a partir desde <address> a <address> + <size> bytes con el caracter <char>\n\n");
+	printf("Shows content in memory of <size> bytes from <address> to <address> + <size> bytes\n\n");
 }
 
 void helpMemory(){
 	printf("\t|memory [-funcs/-vars/-blocks/-all/-pmap]\n");
-	printf("\t[-funcs]: Muestra en pantalla la dirección en memoria de tres funciones del programa y tres functiones de librería\n");
-	printf("\t[-vars]: Muestra en pantalla las direcciones de tres variables de distintos tipos\n");
-	printf("\t[-blocks]: Muestra en pantalla los bloques de memoria asignador con el commando allocate\n");
-	printf("\t[-all]: Muestra en pantalla las tres opciones anteriores\n");
-	printf("\t[-pmap]: Muestra en pantalla el pmap del proceso del shell\n");
+	printf("\t[-funcs]: Prints memory address of three program functions and three library functions\n");
+	printf("\t[-vars]: Prints addresses of variables of different types\n");
+	printf("\t[-blocks]: Prints all memory blocks assigned with allocate command\n");
+	printf("\t[-all]: Prints all all three -funcs -vars and -blocks\n");
+	printf("\t[-pmap]: Show pmap of the shell's process\n");
 }
 
 void helpRecurse(){
-	printf("\t|recurse [N]\n");
-	printf("Función recursiva que almacena dos arrays de 2048 bytes:\nUno automático y otro estático\nMuestra también las direcciones de ambos antes de llamarse a sí misma\n\n");
+	printf("\t|recurse [N] [NOT IMPLEMENTED]\n");
+	printf("Recursive function that stores two arrays of 2048 bytes:\nOne's automatic, the other one's static\nShows addresses of both before calling itself\n\n");
+	//keeping old Spanish description in case the translation must be corrected later TODO: Confirm translation and delete comment
+	//printf("Función recursiva que almacena dos arrays de 2048 bytes:\nUno automático y otro estático\nMuestra también las direcciones de ambos antes de llamarse a sí misma\n\n");
 }
